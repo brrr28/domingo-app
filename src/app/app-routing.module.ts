bb9ef837e3b89e78306155bc3d0580448756f02c
Pavlo Bohachenko <pavlo.bohachenko@nixsolutions.com>
2022-02-01 21:33:31 +0200

    first commit of  markup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: MainPageComponent },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
